b653d4ccb5482dd6eac77fdd5505377352fe9c13
aymeric s <sandoz.aymeric@gmail.com>
2023-02-25 01:33:24 +0100

    ensemble des informations du produits récupérés puis exporter format xlsx

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import csv
 import openpyxl
 import os
+import re
 
 worbook = openpyxl.Workbook()
 books_data = {}
@@ -28,21 +29,62 @@ categorys_a.pop(0)
     
 #  books_data[category.string.strip()]=books_in_one_category
 
-for category in categorys_a[:1]:
+for category in categorys_a[:5]:
  url_page_category = category.get("href")
  page = requests.get(f"http://books.toscrape.com/{url_page_category}")
  soup = BeautifulSoup(page.content, 'html.parser')
  products=soup.find_all('article')
+ category_file = worbook.create_sheet(title=category.string.strip())
 
- for product in products[1:4]:
+
+ coloumn_headers=["product_page_url","universal_product_code","title","price_including_tax","price_excluding_tax","number_available","description","category","review_rating","image_url"]
+ for col, header in enumerate(coloumn_headers, start=1):
+    category_file.cell(row=1, column=col, value=header)
+ 
+ 
+ row=2
+ for product in products:
+  
   products_links=product.find('a')
-  url_products_links=products_links.get("href")
-  url_products_links_normpath = url_products_links.replace("../../../", "")
-  page = requests.get(f"http://books.toscrape.com/catalogue/{url_products_links_normpath}")
-
+  product_page_url=products_links.get("href")
+  product_page_url = product_page_url.replace("../../../", "")
+  page = requests.get(f"http://books.toscrape.com/catalogue/{product_page_url}")
   soup = BeautifulSoup(page.content, 'html.parser')
-  price=soup.find_all('td')
-  print(price)
+  product_information=soup.find(class_='table-striped')
+
+  td=soup.find_all('td')
+  universal_product_code=td[0].string
+  title=soup.find('h1').string
+  price_including_tax=td[2].string
+  price_excluding_tax=td[3].string
+  number_available=re.search(r'\d+', td[5].string).group()
+  product_description_div=soup.find(id="product_description")
+  if product_description_div is not None:
+    product_description = product_description_div.find_next_sibling("p").string
+  else:
+    product_description = "No product description available"
+  review_rating=soup.find(class_="star-rating")["class"][1]
+
+  image_div=soup.find(id="product_gallery")
+  image_url=image_div.find('img')["src"]
+  image_url=image_url.replace("../../", "http://books.toscrape.com/")
+  
+  
+  coloumn_values=[product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category.string.strip(),review_rating,image_url]
+  for col, value in enumerate(coloumn_values, start=1):
+   category_file.cell(row=row, column=col, value=value)
+  row=row+1
+
+# product_page_url
+# ● universal_ product_code (upc)
+# ● title
+# ● price_including_tax
+# ● price_excluding_tax
+# ● number_available
+# ● product_description
+# ● category
+# ● review_rating
+# ● image_url
 
 
 
@@ -72,10 +114,15 @@ for category in categorys_a[:1]:
 #             row=row+1
     
 
-directory = "C:\Users\aymbe\OneDrive\Documents\formation python OC\P2\code\excelFIle"
-filename = "books.xlsx"
-filepath = os.path.join(directory, filename)
-worbook.save(filepath)
+# directory = "C:\Users\aymbe\OneDrive\Documents\formation python OC\P2\code\excelFile"
+# filename = "books.xlsx"
+# filepath = os.path.join(directory, filename)
+# worbook.save(filepath)
+
+
+first_sheet = worbook['Sheet']
+worbook.remove(first_sheet)
+worbook.save("books.xlsx")
 
 
 
