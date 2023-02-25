b5f11e42452b3b372f83006a4fb2b260c91cb32a
aymeric s <sandoz.aymeric@gmail.com>
2023-02-25 11:33:01 +0100

    Récupération sur différentes pages

--- index.py
+++ index.py
@@ -40,48 +40,93 @@ for category in categorys_a[:5]:
  if soup.find(class_="pager") is not None: 
   page_info = pager.find('li', class_='current').text.strip()
   max_page = int(page_info.split()[-1])
-  print(max_page)
  else : 
-   print("oups")
-
+  max_page=1
 
  coloumn_headers=["product_page_url","universal_product_code","title","price_including_tax","price_excluding_tax","number_available","description","category","review_rating","image_url"]
  for col, header in enumerate(coloumn_headers, start=1):
-    category_file.cell(row=1, column=col, value=header)
- 
+  category_file.cell(row=1, column=col, value=header) 
  
  row=2
- for product in products[:3]:
-  
-  products_links=product.find('a')
-  product_page_url=products_links.get("href")
-  product_page_url = product_page_url.replace("../../../", "")
-  page = requests.get(f"http://books.toscrape.com/catalogue/{product_page_url}")
+ for page_number in range(1,max_page+1):
+  url_page_category = category.get("href")
+  print(max_page)
+  if max_page>1:
+   url_page_category=url_page_category.replace("index",f"page-{page_number}")
+  page = requests.get(f"http://books.toscrape.com/{url_page_category}")
+  print(url_page_category)
   soup = BeautifulSoup(page.content, 'html.parser')
-  product_information=soup.find(class_='table-striped')
+  products=soup.find_all('article')
+  for product in products[:3]:
+  
+   products_links=product.find('a')
+   product_page_url=products_links.get("href")
+   product_page_url = product_page_url.replace("../../../", "http://books.toscrape.com/catalogue/")
+   page = requests.get(product_page_url)
+   soup = BeautifulSoup(page.content, 'html.parser')
+   product_information=soup.find(class_='table-striped')
 
-  td=soup.find_all('td')
-  universal_product_code=td[0].string
-  title=soup.find('h1').string
-  price_including_tax=td[2].string
-  price_excluding_tax=td[3].string
-  number_available=re.search(r'\d+', td[5].string).group()
-  product_description_div=soup.find(id="product_description")
-  if product_description_div is not None:
+   td=soup.find_all('td')
+   universal_product_code=td[0].string
+   title=soup.find('h1').string
+   price_including_tax=td[2].string
+   price_excluding_tax=td[3].string
+   number_available=re.search(r'\d+', td[5].string).group()
+   product_description_div=soup.find(id="product_description")
+   if product_description_div is not None:
     product_description = product_description_div.find_next_sibling("p").string
-  else:
+   else:
     product_description = "No product description available"
-  review_rating=soup.find(class_="star-rating")["class"][1]
+   review_rating=soup.find(class_="star-rating")["class"][1]
 
-  image_div=soup.find(id="product_gallery")
-  image_url=image_div.find('img')["src"]
-  image_url=image_url.replace("../../", "http://books.toscrape.com/")
+   image_div=soup.find(id="product_gallery")
+   image_url=image_div.find('img')["src"]
+   image_url=image_url.replace("../../", "http://books.toscrape.com/")
   
   
-  coloumn_values=[product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category.string.strip(),review_rating,image_url]
-  for col, value in enumerate(coloumn_values, start=1):
-   category_file.cell(row=row, column=col, value=value)
-  row=row+1
+   coloumn_values=[product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category.string.strip(),review_rating,image_url]
+   for col, value in enumerate(coloumn_values, start=1):
+    category_file.cell(row=row, column=col, value=value)
+   row=row+1
+
+
+#  coloumn_headers=["product_page_url","universal_product_code","title","price_including_tax","price_excluding_tax","number_available","description","category","review_rating","image_url"]
+#  for col, header in enumerate(coloumn_headers, start=1):
+#   category_file.cell(row=1, column=col, value=header)
+ 
+ 
+#  row=2
+#  for product in products[:3]:
+  
+#   products_links=product.find('a')
+#   product_page_url=products_links.get("href")
+#   product_page_url = product_page_url.replace("../../../", "")
+#   page = requests.get(f"http://books.toscrape.com/catalogue/{product_page_url}")
+#   soup = BeautifulSoup(page.content, 'html.parser')
+#   product_information=soup.find(class_='table-striped')
+
+#   td=soup.find_all('td')
+#   universal_product_code=td[0].string
+#   title=soup.find('h1').string
+#   price_including_tax=td[2].string
+#   price_excluding_tax=td[3].string
+#   number_available=re.search(r'\d+', td[5].string).group()
+#   product_description_div=soup.find(id="product_description")
+#   if product_description_div is not None:
+#     product_description = product_description_div.find_next_sibling("p").string
+#   else:
+#     product_description = "No product description available"
+#   review_rating=soup.find(class_="star-rating")["class"][1]
+
+#   image_div=soup.find(id="product_gallery")
+#   image_url=image_div.find('img')["src"]
+#   image_url=image_url.replace("../../", "http://books.toscrape.com/")
+  
+  
+#   coloumn_values=[product_page_url,universal_product_code,title,price_including_tax,price_excluding_tax,number_available,product_description,category.string.strip(),review_rating,image_url]
+#   for col, value in enumerate(coloumn_values, start=1):
+#    category_file.cell(row=row, column=col, value=value)
+#   row=row+1
 
 # product_page_url
 # ● universal_ product_code (upc)
@@ -128,9 +173,9 @@ for category in categorys_a[:5]:
 # worbook.save(filepath)
 
 
-# first_sheet = worbook['Sheet']
-# worbook.remove(first_sheet)
-# worbook.save("books.xlsx")
+first_sheet = worbook['Sheet']
+worbook.remove(first_sheet)
+worbook.save("books.xlsx")
 
 
 
